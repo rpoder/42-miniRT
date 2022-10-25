fa1aed329b5e20516c019132c2b6b5024c8504bf
Margot POURREY <mpourrey@e1r12p23.clusters.42paris.fr>
2022-10-25 16:19:16 +0200

    ca marche sur cet ordi wallah

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rpoder <rpoder@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mpourrey <mpourrey@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 13:37:48 by rpoder            #+#    #+#             */
-/*   Updated: 2022/10/25 12:57:44 by rpoder           ###   ########.fr       */
+/*   Updated: 2022/10/25 16:17:04 by mpourrey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ int	main(void)
 	t_mlx_data	mlx_data;
 
 	mlx_data.mlx = mlx_init();
-/* 	mlx_data.win = mlx_new_window(mlx_data.mlx, 1000, 1000, "Hello world!");
+ 	mlx_data.win = mlx_new_window(mlx_data.mlx, 1000, 1000, "Hello world!");
 	mlx_data.image.img = mlx_new_image(mlx_data.mlx, 1000, 1000);
 	mlx_data.image.addr = mlx_get_data_addr(mlx_data.image.img,
 		&mlx_data.image.bits_per_pixel, &mlx_data.image.line_length,
-		&mlx_data.image.endian); */
+		&mlx_data.image.endian);
 
 
 	t_ray					ray;
@@ -33,14 +33,14 @@ int	main(void)
 	ray.direction = ft_create_tuple(5, 2, 0, 0);
 	ray.direction = ft_normalize_tuple(ray.direction);
 	ft_print_tuple("ray.direction", ray.direction);
-	/* sphere = ft_create_sphere(NULL, ft_create_tuple(0, 0, 0, 1), 1);
+	 sphere = ft_create_sphere(NULL, ft_create_tuple(0, 0, 0, 1), 1);
 	sphere->transform_m = ft_calculate_translation_matrix(5, 0, 0);
 	sphere_intersects = ft_get_sphere_intersections(sphere, ray);
-	printf("i1 %f | i2 %f\n", sphere_intersects.i1, sphere_intersects.i2); */
+	printf("i1 %f | i2 %f\n", sphere_intersects.i1, sphere_intersects.i2); 
 
 
-	// mlx_put_image_to_window(mlx_data.mlx, mlx_data.win, mlx_data.image.img, 0, 0);
-	// mlx_loop(mlx_data.mlx);
+	mlx_put_image_to_window(mlx_data.mlx, mlx_data.win, mlx_data.image.img, 0, 0);
+	mlx_loop(mlx_data.mlx);
 
 
 }
